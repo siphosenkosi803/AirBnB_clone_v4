a7842dfee0a0e862b67269c1412f488c1f933a34
mumbuamutuku <mumbuamutuku@gmail.com>
2023-05-07 06:02:23 -0700

    app error

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
-//update CORS
+"""update CORS"""
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
